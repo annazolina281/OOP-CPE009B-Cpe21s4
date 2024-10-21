faa0f99975958b6616189dc588601b7fc19492fd
Anna Zolina <qamlzolina@tip.edu.ph>
2024-10-21 10:30:52 +0800

    Create main.py

---
+++ CPE 009 Lab Activity 5 - Introduction to Event Handling in GUI Development/Supplementary Activity/main.py
@@ -0,0 +1,8 @@
+from Registration import MyWindow
+from tkinter import *
+
+window = Tk()
+MyWin=MyWindow(window)
+window.geometry("400x350+10+10")
+window.title("Account Registration System")
+window.mainloop()
